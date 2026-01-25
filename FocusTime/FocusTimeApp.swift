a4e5727ae8c1afc3b9f41822e6d931028417907e
Guyfromtheke <kienjdu@gmail.com>
2026-01-25 17:38:17 +0300

    Initial Commit

---
+++ FocusTime/FocusTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimeApp.swift
+//  FocusTime
+//
+//  Created by Duncan Njoroge on 25/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
